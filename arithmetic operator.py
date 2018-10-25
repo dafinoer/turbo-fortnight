0109ae8b8c289a84e25bfa8b61d43a59228da3bc
amruthajm <43128144+amruthajm@users.noreply.github.com>
2018-10-25 18:20:46 +0530

    Create arithmetic operator.py

---
+++ arithmetic operator.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+    link:https://www.hackerrank.com/challenges/python-arithmetic-operators/submissions/code/88849907
