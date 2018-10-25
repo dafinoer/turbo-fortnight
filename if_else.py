adda497fe1ea992b73c48f53e9f805297dc69051
amruthajm <43128144+amruthajm@users.noreply.github.com>
2018-10-25 17:25:28 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,11 @@
+n = int(raw_input())
+if n%2==0 and 2<=n<=5:
+    print("Not Weird")
+elif n%2==0 and 6<=n<=20:
+    print("Weird")
+elif n%2==0 and n>20:
+    print("Not Weird")
+elif n%2!=0:
+    print("Weird")
+    
+    link:https://www.hackerrank.com/challenges/py-if-else/submissions/code/88849807
